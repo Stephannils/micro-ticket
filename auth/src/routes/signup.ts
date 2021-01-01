64240b7a2a9d99e7ed066513a17cd92dfc96dee9
stephannils <jehle.stephan@yahoo.de>
2021-01-01 09:16:03 +0100

    intgrated express-validator for signup.ts

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,33 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  res.send('Hi there');
-});
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().normalizeEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 8, max: 20 })
+      .withMessage('Password must be between 8 and 20 characters'),
+    body('passwordConfirmation')
+      .equals('password')
+      .withMessage('Passwords need to match'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    console.log('Creating user...');
+
+    res.send({});
+  }
+);
 
 export { router as signupRouter };
